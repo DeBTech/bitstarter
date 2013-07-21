b8ca2b29770edabcc19fb43f5157d9fc7582bcde
DeBTech <debtech@gmail.com>
2013-07-21 15:32:05 -0500

    Integrate local bootstrap.

--- web.js
+++ web.js
@@ -3,12 +3,14 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname));
+
 app.get('/', function(request, response) {
-	var fileText = fs.readFileSync('index.html').toString();
+  var fileText = fs.readFileSync('index.html').toString();
   response.send(fileText);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
